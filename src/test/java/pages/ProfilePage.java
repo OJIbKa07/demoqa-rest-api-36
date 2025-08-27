15aba34a4f80a920de8057847a21f85e02c89d4f
Olga Palushina <lokipypzemli@mail.ru>
2025-08-27 21:35:39 +0300

    add selenoid in TestBase

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -1,18 +1,22 @@
 package pages;
 
 import api.DeleteBookApi;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static tests.TestData.USERNAME;
 
 public class ProfilePage {
 
     private SelenideElement
-            usernameLabel = $("#userName-value");
+            usernameLabel = $("#userName-value"),
+            deleteButton = $("div.text-right.button.di button"),
+            modalWindow =  $("#closeSmallModal-ok"),
+            listMyBook = $(".rt-noData");
+
 
     @Step("Проверяем успешность авторизации")
     public ProfilePage checkAuthorization() {
@@ -22,9 +26,25 @@ public class ProfilePage {
         return this;
     }
 
-    @Step("Удаляем книгу из корзины")
+    @Step("Удаляем книгу из корзины по API")
     public ProfilePage deleteBook() {
         DeleteBookApi.deleteBookFromProfileTest();
+
+        return this;
+    }
+
+    @Step("Удаляем книгу из корзины в UI")
+    public ProfilePage clickDeleteAllBooks() {
+        deleteButton.click();
+        modalWindow.shouldBe(Condition.visible)
+                .click();
+        switchTo().alert().accept();
+
+        return this;
+    }
+
+    public ProfilePage checkDeleteBook() {
+        listMyBook.shouldBe(Condition.visible);
 
         return this;
     }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,18 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import java.util.Map;
+
 
 public class TestBase {
     @BeforeAll
@@ -13,11 +20,28 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1980x1080";
+        Configuration.browser =  System.getProperty ("browser","chrome");
+        Configuration.browserVersion =  System.getProperty("browser_version", "127.0");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url","selenoid.autotests.cloud/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListenerAndRuCookie() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
-    void shutDown() {
-        closeWebDriver();
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
